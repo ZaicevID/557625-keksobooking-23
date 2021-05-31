5a3efce281c100b6249dc74a3a7b115df8388d4a
Илья <zaicev.id@mail.ru>
2021-05-31 10:09:37 +0300

    Синтаксические правки

--- js/main.js
+++ js/main.js
@@ -1,27 +1,23 @@
-let randomNumber = function (min, max) {
+const RANDOM_NUMBER = function (min, max) {
   if (min<0 || max<0) {
-    return "Диапазон может быть только положительный"
+    return 'Диапазон может быть только положительный';
   }
-
   if (min>=max) {
-    return "Максимальное значение меньше либо равно минимальному"
+    return 'Максимальное значение меньше либо равно минимальному';
   }
-
   return Math.round(Math.random() * (max - min)) + min;
-}
+};
 
-randomNumber(1, 3);
+RANDOM_NUMBER(1, 3);
 
-let floatingPointNumber = function (min, max, signs) {
+const FLOATING_POINT_NUMBER = function (min, max, signs) {
   if (min<0 || max<0) {
-    return "Диапазон может быть только положительный"
+    return 'Диапазон может быть только положительный';
   }
-
   if (min>=max) {
-    return "Максимальное значение меньше либо равно минимальному"
+    return 'Максимальное значение меньше либо равно минимальному';
   }
-
   return (Math.random() * (max - min) + min).toFixed(signs);
-}
+};
 
-floatingPointNumber(1, 3, 3);
+FLOATING_POINT_NUMBER(1, 3, 3);
