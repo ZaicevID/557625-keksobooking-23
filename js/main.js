d48c3f505b35ce5c782ea48c4cdcc645f65d400a
Илья <zaicev.id@mail.ru>
2021-06-05 17:25:17 +0300

    Добавляет массив с 10 объектами

--- js/main.js
+++ js/main.js
@@ -1,20 +1,120 @@
-let getRandomNumber = function (min, max) {
-  if (min<0 || max<0) {
-    throw new Error('Диапазон может быть только положительный');
-  }
-  if (min>=max) {
-    throw new Error('Максимальное значение меньше либо равно минимальному');
-  }
-  return Math.round(Math.random() * (max - min)) + min;
+// Функция взята из интернета и доработана
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+
+function getRandomPositiveFloat (a, b, digits = 1) {
+  // Чтобы не заставлять пользователя нашей функции помнить порядок аргументов,
+  // реализуем поддержку передачи минимального и максимального значения в любом порядке,
+  // а какое из них большее и меньшее вычислим с помощью Math.min и Math.max
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+  // Обратите внимание, чтобы учесть условие, что диапазон может быть [0, ∞),
+  // мы не ругаем пользователя за переданное отрицательное число,
+  // а просто берём его по модулю с помощью Math.abs
+
+  // Дальше используем Math.random() для получения случайного дробного числа в диапазоне [0, 1),
+  // которое домножаем на разницу между переданными числами - это будет наша случайная дельта.
+  // После нужно сложить дельту с минимальным значением, чтобы получить итоговое случайное число.
+  const result = Math.random() * (upper - lower) + lower;
+
+  // И в конце с помощью метода toFixed любого числа в JavaScript
+  // указать требуемое количество знаков после точки
+  return result.toFixed(digits);
+}
+// Функция взята из интернета и доработана
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+
+function getRandomPositiveInteger (a, b) {
+  // Чтобы не заставлять пользователя нашей функции помнить порядок аргументов,
+  // реализуем поддержку передачи минимального и максимального значения в любом порядке,
+  // а какое из них большее и меньшее вычислим с помощью Math.min и Math.max.
+
+  // После нам нужно убедиться, что пользователь не передал дробные значения,
+  // для этого на всякий пожарный случай нижнюю границу диапазона
+  // мы округляем к ближайшему большему целому с помощью Math.ceil,
+  // а верхнюю границу - к ближайшему меньшему целому с помощью Math.floor
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  // Обратите внимание, чтобы учесть условие, что диапазон может быть [0, ∞),
+  // мы не ругаем пользователя за переданное отрицательное число,
+  // а просто берём его по модулю с помощью Math.abs
+
+  // Дальше используем Math.random() для получения случайного дробного числа в диапазоне [0, 1),
+  // которое домножаем на разницу между переданными числами плюс единица - это будет наша случайная дельта.
+  // После нужно сложить дельту с минимальным значением, чтобы получить итоговое случайное число.
+  const result = Math.random() * (upper - lower + 1) + lower;
+  // "Плюс единица", чтобы включить верхнюю границу диапазона в случайные числа
+
+  // И в конце с помощью метода Math.floor мы округляем полученный результат,
+  // потому что Math.random() генерирует только дробные числа и ноль.
+  return Math.floor(result);
 };
 
-let getFloatingPointNumber = function (min, max, signs) {
-  if (min<0 || max<0) {
-    throw new Error('Диапазон может быть только положительный');
-  }
-  if (min>=max) {
-    throw new Error('Максимальное значение меньше либо равно минимальному');
-  }
-  return (Math.random() * (max - min) + min).toFixed(signs);
+
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel'
+];
+
+const CHECKIN = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+const CHECKOUT = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+
+let seller = {
+  avatar: 'img/avatars/user0' + getRandomPositiveInteger(1, 8) + '.png'
 };
+
+let coordinates = {
+  lat: getRandomPositiveFloat(35.65000, 35.70000, 5),
+  lng: getRandomPositiveFloat(139.70000, 139.80000, 5)
+};
+
+let prices = getRandomPositiveInteger(100000, 1000000);
+
+let product = {
+  title: 'Предложение' + ' ' +getRandomPositiveInteger(1, 10),
+  address: coordinates.lat + ' ' + coordinates.lng,
+  price: getRandomPositiveInteger(100000, 1000000),
+  type: TYPES[getRandomPositiveInteger(0, TYPES.length - 1)],
+  rooms: getRandomPositiveInteger(1, 5),
+  guests: getRandomPositiveInteger(5, 10),
+  checkin: CHECKIN[getRandomPositiveInteger(0, CHECKIN.length - 1)],
+  checkout: CHECKOUT[getRandomPositiveInteger(0, CHECKOUT.length - 1)],
+  features: 'не понимаю',
+  description: 'Описание' + ' ' + getRandomPositiveInteger(1, 10),
+  photos: 'не понимаю'
+};
+
+const createOffer = () => {
+  return {
+    author: seller,
+    offer: product,
+    location: coordinates
+  };
+};
+
+const newOffer = new Array(10).fill(null).map(() => createOffer());
+
+
+
+
 
